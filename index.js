41730d5a9711659e6a0b31737b63e752b979fc55
Meka Seba <mekasebameka@gmail.com>
2022-07-12 23:16:59 -0700

    confused

---
+++ index.js
@@ -0,0 +1,20 @@
+let hOne = document.getElementById("h-one")
+let home = document.getElementById("home")
+let home = 0
+
+function hOne() {
+    hOne += 1
+    hOne.textContent = home
+}
+
+
+
+
+/*let homeScoreBtnOne = document.getElementById("home-score-btn-1")
+let homeStoreEl =document.getElementById("home-score")
+let homeScore = 0
+
+function increaseHomeScoreOne(){
+    homeScore += 1
+    homeStoreEl.textContent = homeScore
+}
