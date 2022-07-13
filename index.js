9434720e4b78028242c594d689c5794060263ad1
Meka Seba <mekasebameka@gmail.com>
2022-07-12 23:48:16 -0700

    mas

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let homeScoreElement = document.getElementById("homeScoreElement")
+let homeScoreElement = document.getElementById("home-score-element")
+let guestScoreElement = document.getElementById("guest-score-element")
+
 let home = document.getElementById("home")
 let home = 0
 
